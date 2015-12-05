4a5780d9e937bc8c337645d8a7703468a9d32593
Matthew Wiecek <matthewwiecek@tamu.edu>
2015-12-05 16:44:29 -0600

    Solver should hopefully be able to handle larger inputs, parsing the input in chunks.

--- Solver.java
+++ Solver.java
@@ -6,6 +6,7 @@ import java.util.regex.*;
 import aima.core.logic.propositional.inference.DPLL;
 import aima.core.logic.propositional.inference.WalkSAT;
 import aima.core.logic.propositional.inference.OptimizedDPLL;
+import aima.core.logic.propositional.kb.KnowledgeBase;
 import aima.core.logic.propositional.kb.data.Clause;
 import aima.core.logic.propositional.kb.data.Model;
 import aima.core.logic.propositional.parsing.PLParser;
@@ -37,12 +38,62 @@ public class Solver {
 		}
 	}
 	
+	static ArrayList<String> dpll(ArrayList<String> toParse)
+	{
+		DPLL dpll = new OptimizedDPLL();
+		PLParser parser = new PLParser();
+		Model model = new Model();
+		KnowledgeBase kb = new KnowledgeBase();
+		for(String a : toParse)
+		{
+			kb.tell(a);
+		}
+		Sentence sentence = kb.asSentence();
+		Set <Clause> clauses =  ConvertToConjunctionOfClauses.convert(sentence).getClauses();
+		List<PropositionSymbol> symbols = new ArrayList<PropositionSymbol>(
+					SymbolCollector.getSymbolsFrom(sentence));
+		boolean succeded = dpll.dpll(clauses,symbols, model);
+		if(succeded)
+		{
+			return generateTrueList(model, symbols);
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
 	static ArrayList<String> walkSAT(String toParse)
 	{
 		final int give_up = 100000;
 		WalkSAT walk = new WalkSAT();
 		PLParser parser = new PLParser();
 		Sentence sentence = parser.parse(toParse);
+		Set <Clause> clauses =  ConvertToConjunctionOfClauses.convert(sentence).getClauses();
+		Model model = walk.walkSAT(clauses, 0.5, give_up);
+		if(model == null)
+		{
+			return null;
+		}
+		else
+		{	
+			List<PropositionSymbol> symbols = new ArrayList<PropositionSymbol>(
+					SymbolCollector.getSymbolsFrom(sentence));
+			return generateTrueList(model, symbols);
+		}
+	}
+	
+	static ArrayList<String> walkSAT(ArrayList<String> toParse)
+	{
+		final int give_up = 100000;
+		WalkSAT walk = new WalkSAT();
+		PLParser parser = new PLParser();
+		KnowledgeBase kb = new KnowledgeBase();
+		for(String a : toParse)
+		{
+			kb.tell(a);
+		}
+		Sentence sentence = kb.asSentence();
 		Set <Clause> clauses =  ConvertToConjunctionOfClauses.convert(sentence).getClauses();
 		Model model = walk.walkSAT(clauses, 0.5, give_up);
 		if(model == null)
