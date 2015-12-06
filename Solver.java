715055fe6a9657a1f1bf14e7bec0c7fddc43a50b
Matthew Wiecek <matthewwiecek@tamu.edu>
2015-12-05 19:06:18 -0600

    Solver now uses Knuths SAT solver.

--- Solver.java
+++ Solver.java
@@ -1,124 +1,62 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.regex.*;
-
-import aima.core.logic.propositional.inference.DPLL;
-import aima.core.logic.propositional.inference.WalkSAT;
-import aima.core.logic.propositional.inference.OptimizedDPLL;
-import aima.core.logic.propositional.kb.KnowledgeBase;
-import aima.core.logic.propositional.kb.data.Clause;
-import aima.core.logic.propositional.kb.data.Model;
-import aima.core.logic.propositional.parsing.PLParser;
-import aima.core.logic.propositional.parsing.ast.PropositionSymbol;
-import aima.core.logic.propositional.parsing.ast.Sentence;
-import aima.core.logic.propositional.visitors.ConvertToConjunctionOfClauses;
-import aima.core.logic.propositional.visitors.SymbolCollector;
-
+import java.util.Scanner;
+import java.lang.Process;
+import java.lang.ProcessBuilder;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Solver {
 
-	static ArrayList<String> dpll(String toParse)
+	static String SAT(String input) throws IOException
 	{
-		DPLL dpll = new OptimizedDPLL();
-		PLParser parser = new PLParser();
-		Model model = new Model();
-		Sentence sentence = parser.parse(toParse);
-		Set <Clause> clauses =  ConvertToConjunctionOfClauses.convert(sentence).getClauses();
-		List<PropositionSymbol> symbols = new ArrayList<PropositionSymbol>(
-					SymbolCollector.getSymbolsFrom(sentence));
-		boolean succeded = dpll.dpll(clauses,symbols, model);
-		if(succeded)
-		{
-			return generateTrueList(model, symbols);
-		}
-		else
-		{
-			return null;
-		}
+		Process process = new ProcessBuilder("./SAT").start();
+		OutputStream stdin = process.getOutputStream();
+		InputStream stdout = process.getInputStream();
+		
+		//BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+		
+		writer.write(input);
+		writer.write("\n");
+		writer.flush();
+		writer.close();
+		
+		Scanner scan = new Scanner(stdout);
+		
+		return scan.nextLine();
 	}
-	
-	static ArrayList<String> dpll(ArrayList<String> toParse)
+
+}
+import java.util.Scanner;
+import java.lang.Process;
+import java.lang.ProcessBuilder;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Solver {
+
+	static String SAT(String input) throws IOException
 	{
-		DPLL dpll = new OptimizedDPLL();
-		PLParser parser = new PLParser();
-		Model model = new Model();
-		KnowledgeBase kb = new KnowledgeBase();
-		for(String a : toParse)
-		{
-			kb.tell(a);
-		}
-		Sentence sentence = kb.asSentence();
-		Set <Clause> clauses =  ConvertToConjunctionOfClauses.convert(sentence).getClauses();
-		List<PropositionSymbol> symbols = new ArrayList<PropositionSymbol>(
-					SymbolCollector.getSymbolsFrom(sentence));
-		boolean succeded = dpll.dpll(clauses,symbols, model);
-		if(succeded)
-		{
-			return generateTrueList(model, symbols);
-		}
-		else
-		{
-			return null;
-		}
-	}
-	
-	static ArrayList<String> walkSAT(String toParse)
-	{
-		final int give_up = 100000;
-		WalkSAT walk = new WalkSAT();
-		PLParser parser = new PLParser();
-		Sentence sentence = parser.parse(toParse);
-		Set <Clause> clauses =  ConvertToConjunctionOfClauses.convert(sentence).getClauses();
-		Model model = walk.walkSAT(clauses, 0.5, give_up);
-		if(model == null)
-		{
-			return null;
-		}
-		else
-		{	
-			List<PropositionSymbol> symbols = new ArrayList<PropositionSymbol>(
-					SymbolCollector.getSymbolsFrom(sentence));
-			return generateTrueList(model, symbols);
-		}
-	}
-	
-	static ArrayList<String> walkSAT(ArrayList<String> toParse)
-	{
-		final int give_up = 100000;
-		WalkSAT walk = new WalkSAT();
-		PLParser parser = new PLParser();
-		KnowledgeBase kb = new KnowledgeBase();
-		for(String a : toParse)
-		{
-			kb.tell(a);
-		}
-		Sentence sentence = kb.asSentence();
-		Set <Clause> clauses =  ConvertToConjunctionOfClauses.convert(sentence).getClauses();
-		Model model = walk.walkSAT(clauses, 0.5, give_up);
-		if(model == null)
-		{
-			return null;
-		}
-		else
-		{	
-			List<PropositionSymbol> symbols = new ArrayList<PropositionSymbol>(
-					SymbolCollector.getSymbolsFrom(sentence));
-			return generateTrueList(model, symbols);
-		}
-	}
-	
-	static ArrayList<String> generateTrueList(Model model, List<PropositionSymbol> symbols)
-	{
-		ArrayList<String> to_return = new ArrayList<String>();
-		for(PropositionSymbol a : symbols)
-		{
-			if(model.isTrue(a))
-			{
-				to_return.add(a.toString());
-			}
-		}
-		return to_return;
+		Process process = new ProcessBuilder("./SAT").start();
+		OutputStream stdin = process.getOutputStream();
+		InputStream stdout = process.getInputStream();
+		
+		//BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+		
+		writer.write(input);
+		writer.write("\n");
+		writer.flush();
+		writer.close();
+		
+		Scanner scan = new Scanner(stdout);
+		
+		return scan.nextLine();
 	}
 
 }
